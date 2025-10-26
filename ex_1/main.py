bb494a91df2581679c20bca8d1fc76aa96ffdc4c
Jesse3141 <yishaibz@gmail.com>
2025-10-26 14:18:34 +0200

    first commit

---
+++ ex_1/main.py
@@ -0,0 +1,82 @@
+
+def Scenario_1():
+    """
+    Question 1.
+    2 Prophets 1 Game.
+    You may change the input & output parameters of the function as you wish.
+    """
+    ############### YOUR CODE GOES HERE ###############
+    pass
+
+
+def Scenario_2():
+    """
+    Question 2.
+    2 Prophets 10 Games.
+    You may change the input & output parameters of the function as you wish.
+    """
+    ############### YOUR CODE GOES HERE ###############
+    pass
+
+
+def Scenario_3():
+    """
+    Question 3.
+    500 Prophets 10 Games.
+    You may change the input & output parameters of the function as you wish.
+    """
+    ############### YOUR CODE GOES HERE ###############
+    pass
+
+
+def Scenario_4():
+    """
+    Question 4.
+    500 Prophets 1000 Games.
+    You may change the input & output parameters of the function as you wish.
+    """
+    ############### YOUR CODE GOES HERE ###############
+    pass
+
+
+def Scenario_5():
+    """
+    Question 5.
+    School of Prophets.
+    You may change the input & output parameters of the function as you wish.
+    """
+    ############### YOUR CODE GOES HERE ###############
+    pass
+
+
+def Scenario_6():
+    """
+    Question 6.
+    The Bias-Variance Tradeoff.
+    You may change the input & output parameters of the function as you wish.
+    """
+    ############### YOUR CODE GOES HERE ###############
+    pass
+
+
+if __name__ == '__main__':
+    
+
+    print(f'Scenario 1 Results:')
+    Scenario_1()
+
+    print(f'Scenario 2 Results:')
+    Scenario_2()
+
+    print(f'Scenario 3 Results:')
+    Scenario_3()
+
+    print(f'Scenario 4 Results:')
+    Scenario_4()
+
+    print(f'Scenario 5 Results:')
+    Scenario_5()
+
+    print(f'Scenario 6 Results:')
+    Scenario_6()
+
